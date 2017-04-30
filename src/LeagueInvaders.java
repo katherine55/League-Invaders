53a5e594f40ff82ba3b57bf045a67d6ae1b4e49e
katherine55 <league@WTS19.attlocal.net>
2017-04-30 11:31:21 -0700

    new
    4/30

--- src/LeagueInvaders.java
+++ src/LeagueInvaders.java
@@ -1,29 +1,30 @@
 import javax.swing.JFrame;
 
 public class LeagueInvaders {
-JFrame myFrame;
-static final int width= 500;
-static final int height= 800;
-GamePanel mygamepanel;
+	JFrame myFrame;
+	static final int width = 500;
+	static final int height = 800;
+	GamePanel mygamepanel;
+
 	public LeagueInvaders() {
 		mygamepanel = new GamePanel();
 		myFrame = new JFrame("League Invaders");
-myFrame.setSize(width, height);
-	}
-	
-private void setup() {
-myFrame.add(mygamepanel);
-myFrame.setVisible(true);
-myFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-mygamepanel.startGame();
-
-myFrame.addKeyListener(mygamepanel);
+		myFrame.setSize(width, height);
 	}
 
-public static void main(String[] args) {
-	
-LeagueInvaders object = new LeagueInvaders();
-	object.setup();
-}	
+	private void setup() {
+		myFrame.add(mygamepanel);
+		myFrame.setVisible(true);
+		myFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		mygamepanel.startGame();
+
+		myFrame.addKeyListener(mygamepanel);
+	}
+
+	public static void main(String[] args) {
+
+		LeagueInvaders object = new LeagueInvaders();
+		object.setup();
+	}
 
 }
